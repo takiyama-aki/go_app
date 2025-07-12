4fec4933fc2ae0a44db0d4605866bb8a4bdeb42b
akki <takiyamaakihito@gmail.com>
2025-07-12 23:00:44 +0900

    簡単なwebページ作成

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,18 @@
+import { Routes, Route, Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header className="h-14 flex items-center px-4 shadow">
+      <h1 className="font-bold">Go&nbsp;Trade&nbsp;App</h1>
+              <nav className="space-x-4">
+          <Link to="/" className="text-blue-600 hover:underline">
+            Home
+          </Link>
+          <Link to="/about" className="text-blue-600 hover:underline">
+            About
+          </Link>
+        </nav>
+
+    </header>
+  );
+}
